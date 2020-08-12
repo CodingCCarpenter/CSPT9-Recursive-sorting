eea47c5644a4e4f7632fdbe821c418244aaf145d
CodingCCarpenter <Christineassists@aol.com>
2020-08-11 20:42:01 -0500

    implement a recursive implementation of binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,19 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
-
+    # base case
+    if end >= start:
+        mid = (end + start)
+        # fetch middle elem of list and returns it if target
+        if arr[mid] == target:
+            return mid
+        # if elem is < mid, search left subarray
+        elif arr[mid] > target:
+            return binary_search(arr, target, start, mid - 1)
+        #else search right subarray
+        else:
+            return binary_search(arr, target, end, mid + 1)
+    else:
+        return -1
 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
@@ -11,4 +23,4 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
-
+    pass
